2ac4bc7cbe1da3f27abc7c83811b58853eb565cf
Passifloram <69451829+passifloram@users.noreply.github.com>
2026-09-01 16:38:39 +0200

    Update badges.js

--- badges.js
+++ badges.js
@@ -135,6 +135,7 @@
         'trophycb-1': ['Trophée CB', 'divers/trophycb-1.png'],
         'reward-crown': ['Le sang de la veine', 'divers/reward-crown.png'],
         'reward-crown-2': ['Le sang de l artère', 'divers/reward-crown-2.png'],
+         'reward-crown-3': ['le sang de la jugulaire', 'divers/reward-crown-3.png'],
 
         'reward-welcome-wagon': ['Welcome Wagon', 'flood-et-vie-du-forum/welcome-wagon.png'],
         'reward-take-a-chance-on-me': ['Take a Chance on Me', 'flood-et-vie-du-forum/take-a-chance-on-me.png'],
